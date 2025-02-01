15063290075008559cd876eaaa0e2a714852fc14
Rodolfo Carvalho <rhcarvalho@gmail.com>
2025-02-01 20:28:10 +0100

    Add alternative SubarraySum.count/1 implementation
    Try to fix the original version to account for multiple subarrays with
    the same sum.
    
    Add alternative implementation using a single pass.

--- 20250202/3-subarray-sum-equals-k/subarray_sum.exs
+++ 20250202/3-subarray-sum-equals-k/subarray_sum.exs
@@ -1,6 +1,7 @@
 ExUnit.start()
 
 defmodule SubarraySumTest do
+  require Logger
   use ExUnit.Case
 
   test "leetcode_examples" do
@@ -11,6 +12,36 @@ defmodule SubarraySumTest do
   test "extra_examples" do
     assert SubarraySum.count([1, 2, 3, 4, 5], 10) == 1
     assert SubarraySum.count([1, 1, 1], 1) == 3
+  end
+
+  test "compare" do
+    nums = Enum.map(1..10_000, fn _ -> Enum.random(-1_000..1_000) end)
+    k = Enum.random(-10_000_000..10_000_000)
+
+    {time_slow, result_slow} = :timer.tc(fn -> SubarraySum.count(nums, k) end)
+    {time_fast, result_fast} = :timer.tc(fn -> SubarraySum.count_single_pass(nums, k) end)
+
+    assert time_fast < time_slow
+    assert result_fast == result_slow
+
+    format_thousands = fn time, padding ->
+      time
+      |> Integer.to_charlist()
+      |> Enum.reverse()
+      |> Enum.chunk_every(3)
+      |> Enum.join(",")
+      |> String.reverse()
+      |> String.pad_leading(padding)
+    end
+
+    """
+    Subarray Sum Equals k comparison
+
+    Slow:    #{time_slow |> format_thousands.(10)}µs
+    Fast:    #{time_fast |> format_thousands.(10)}µs
+    Speedup: #{round(time_slow / time_fast) |> format_thousands.(10)}x
+    """
+    |> Logger.info()
   end
 end
 
@@ -24,7 +55,23 @@ defmodule SubarraySum do
       cumulative_sum
       |> Enum.map(&(&1 - k))
 
-    set = MapSet.new(cumulative_sum)
-    Enum.count(cumulative_sum_minus_k, &MapSet.member?(set, &1))
+    frequency =
+      cumulative_sum
+      |> Enum.reduce(%{}, fn sum, acc ->
+        Map.update(acc, sum, 1, &(&1 + 1))
+      end)
+
+    Enum.sum_by(cumulative_sum_minus_k, &Map.get(frequency, &1, 0))
+  end
+
+  def count_single_pass(nums, k) do
+    nums
+    |> Enum.reduce({%{0 => 1}, 0, 0}, fn num, {cum_sum_seen, cum_sum, count} ->
+      cum_sum = cum_sum + num
+      count = count + Map.get(cum_sum_seen, cum_sum - k, 0)
+      cum_sum_seen = Map.update(cum_sum_seen, cum_sum, 1, &(&1 + 1))
+      {cum_sum_seen, cum_sum, count}
+    end)
+    |> elem(2)
   end
 end
