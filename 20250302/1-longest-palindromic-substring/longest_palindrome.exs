73716d6eb9ed99b7a99946ea8061fdfa9693cafe
Rodolfo Carvalho <rhcarvalho@gmail.com>
2025-03-01 22:09:21 +0100

    Optimize Longest Palindromic Substring

--- 20250302/1-longest-palindromic-substring/longest_palindrome.exs
+++ 20250302/1-longest-palindromic-substring/longest_palindrome.exs
@@ -12,19 +12,29 @@ defmodule LongestPalindromeTest do
     assert LongestPalindrome.find("a") == "a"
     assert LongestPalindrome.find("ac") == "a"
     assert LongestPalindrome.find("aca") == "aca"
+    thousand_as = String.duplicate("a", 1000)
+    assert LongestPalindrome.find(thousand_as) == thousand_as
+    many_bs = String.duplicate("b", 598)
+    many_bs_with_cd = String.duplicate("b", 400) <> "cd" <> many_bs
+    assert LongestPalindrome.find(many_bs_with_cd) == many_bs
   end
 end
 
 defmodule LongestPalindrome do
   def find(s) do
-    for i <- 0..String.length(s), j <- 0..String.length(s), j > i do
-      String.slice(s, i, j)
+    for i <- (byte_size(s) - 1)..0//-1, j <- i..(byte_size(s) - 1)//1, reduce: MapSet.new() do
+      palindromes ->
+        if palindrome?(s, palindromes, i, j) do
+          MapSet.put(palindromes, {i, j})
+        else
+          palindromes
+        end
     end
-    |> Enum.sort_by(&byte_size/1, :desc)
-    |> Enum.find(&palindrome?/1)
+    |> Enum.min_by(fn {i, j} -> {i - j, i} end)
+    |> then(fn {i, j} -> binary_slice(s, i..j) end)
   end
 
-  defp palindrome?(s) do
-    s == String.reverse(s)
+  defp palindrome?(s, p, i, j) do
+    (j - i < 2 || MapSet.member?(p, {i + 1, j - 1})) && :binary.at(s, i) == :binary.at(s, j)
   end
 end
